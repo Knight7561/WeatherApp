b798d7e05d77d7dec480a721f185539030644546
Knight7561 <rohithvr3@gmail.com>
2023-07-25 17:10:20 -0400

    Env update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+  weatherApiBaseUrl:"https://open-weather13.p.rapidapi.com/city/",
+  XRapidAPIHost_label: 'X-RapidAPI-Host',
+  XRapidAPIHost_Value: 'open-weather13.p.rapidapi.com',
+  XRapidAPIKey_Label: 'X-RapidAPI-Key',
+  XRapidAPIKey_Value: 'X',
 };
